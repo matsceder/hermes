553867a5e944e988ecd1da06db30aadfefa7bbf2
matsceder <mats_ceder@hotmail.com>
2025-01-25 21:54:23 +0100

    Add homepage and share text view with basic templates

--- core/views.py
+++ core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, 'core/home.html')  # Lägg till denna funktion
 
+def share_text(request):
+    if request.method == 'POST':
+        # Placeholder: Här hanterar vi formulärdata senare
+        text = request.POST.get('text')
+        expiration = request.POST.get('expiration')
+        return render(request, 'core/share_text.html', {'success': True})
 
-
-
-
-
-
+    return render(request, 'core/share_text.html')
